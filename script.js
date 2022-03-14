fb28da274ce2c4172add03da6fa90bd48bf4c273
sauravjain123 <101493555+sauravjain123@users.noreply.github.com>
2022-03-14 23:05:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+console.log("welcome to tic tac toe");
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("beep-01a.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X";
+let isGameover = false
+
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+}
+
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext')
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && boxtext[e[0]].innerText !== "") {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + ' won';
+            gameover = true;
+
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '56px'
+            gameover.play()
+        }
+
+
+    })
+}
+
+// music.play();
+let boxes = document.getElementsByClassName("box")
+Array.from(boxes).forEach(Element => {
+    let boxtext = Element.querySelector('.boxtext')
+    Element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isGameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+        }
+    })
+    reset.addEventListener('click', () => {
+        let boxtexts = document.querySelectorAll('.boxtext');
+        Array.from(boxtexts).forEach(element => {
+            element.innerText = "";
+
+        });
+        turn = 'X'
+        isGameover = false;
+        document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    
+    })
+})
